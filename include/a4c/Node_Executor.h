bc2418ce1fab7ffc47d180f30bb0d94971e8c640
Voldgalf <50calibursword@gmail.com>
2026-07-04 10:14:45 -0400

    feat: added async mapping
    Allows the developer to pass a lambda function and a vector. This will call the passed function for each vector item, asynchronously.

--- include/a4c/Node_Executor.h
+++ include/a4c/Node_Executor.h
@@ -31,6 +31,22 @@ public:
 
     bool create_async_node(const std::vector<std::function<void(nlohmann::json &)> > &async_nodes);
 
+    template<typename T_input, typename T_output>
+    bool map_async(std::function<T_output(T_input)> base_node, std::vector<T_input> input_vector) {
+        std::vector<std::future<T_output> > logic_futures;
+
+
+        for (T_input input_item: input_vector) {
+            logic_futures.push_back(std::async(std::launch::async, base_node, input_item));
+        }
+
+        for (std::future<T_output> &future: logic_futures) {
+            future.get();
+        }
+
+        return true;
+    }
+
     bool create_sync_node(const std::function<void(nlohmann::json &)> &sync_node);
 
     bool run();
