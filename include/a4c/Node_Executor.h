15f2332f4b93315efc55f57e0bce4df041b90c43
Voldgalf <50calibursword@gmail.com>
2026-07-05 09:24:52 -0400

    feat(headers)!: added ```a4c``` namespace as well as a sub-namespace ```node```.
    Renamed:
    - node_type (enum)
    - node (struct)
    - lambda_node (type alias)
    
    removed:
    - create_sync_node (function)
    - create_async_node (function)
    
    created:
    - add_node (function)

--- include/a4c/Node_Executor.h
+++ include/a4c/Node_Executor.h
@@ -10,51 +10,67 @@
 
 #include "../nlohmann/json.hpp"
 
-using lambda_node = std::function<nlohmann::json(nlohmann::json state)>;
+namespace a4c {
+namespace node {
 
-enum node_type {
-    SYNC,
-    ASYNC,
+using function_alias = std::function<nlohmann::json(nlohmann::json state)>;
+
+enum execution_mode {
+  SEQUENTIAL,
+  CONCURRENT,
 };
 
-struct node {
-    node_type type;
-    lambda_node sync_logic;
-    std::vector<lambda_node> async_logic;
+enum logic_status
+{
+  IDLE,
+  SUCCESS,
+  FAILURE,
 };
+
+struct logic
+{
+  logic_status status;
+  function_alias function;
+};
+
+struct definition {
+  execution_mode mode;
+  std::vector<logic> logic_vector;
+};
+}
 
 class node_executor {
 protected:
-    std::vector<node> nodes_;
-    nlohmann::json state_;
+  std::vector<node::definition> nodes_;
+  nlohmann::json state_;
 
 public:
-    node_executor() = default;
+  node_executor() = default;
 
 
-    bool create_async_node(const std::vector<lambda_node> &async_nodes);
-
-    template<typename TInput, typename TOutput>
-    bool map_async(std::function<TOutput(TInput)> base_node, std::vector<TInput> input_vector) {
-        std::vector<std::future<TOutput> > logic_futures;
+  template<typename TInput, typename TOutput>
+  bool map_async(std::function<TOutput(TInput)> base_node, std::vector<TInput> input_vector) {
+    std::vector<std::future<TOutput> > logic_futures;
 
 
-        for (TInput input_item: input_vector) {
-            nlohmann::json state_copy = state_;
+    for (TInput input_item: input_vector) {
+        nlohmann::json state_copy = state_;
 
-            logic_futures.push_back(std::async(std::launch::async, base_node, input_item));
-        }
-
-        for (std::future<TOutput> &future: logic_futures) {
-            state_.merge_patch(future.get());
-        }
-
-        return true;
+        logic_futures.push_back(std::async(std::launch::async, base_node, input_item));
     }
 
-    bool create_sync_node(const lambda_node &sync_node);
+    for (std::future<TOutput> &future: logic_futures) {
+        state_.merge_patch(future.get());
+    }
 
-    bool run();
+    return true;
+  }
 
-    nlohmann::json get_state() const;
+
+  bool add_node(std::vector<node::function_alias> nodes,node::execution_mode mode_flag);
+
+  bool run();
+
+  nlohmann::json get_state() const;
 };
+}
