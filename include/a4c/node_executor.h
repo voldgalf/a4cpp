b5a47f846c17dcabfca48a0856e2c2f3ee16a12a
Voldgalf <50calibursword@gmail.com>
2026-07-05 19:17:23 -0400

    feat!: removed map_async function

--- include/a4c/node_executor.h
+++ include/a4c/node_executor.h
@@ -24,35 +24,6 @@ public:
     SPDLOG_INFO("node_executor created successfully.");
   }
 
-  template<typename TInput, typename TOutput>
-  bool map_async(std::function<TOutput(TInput)> base_node, std::vector<TInput> input_vector) {
-    try
-      {
-        
-        std::vector<std::future<TOutput> > logic_futures;
-
-
-        for (int i = 0; i < input_vector.size(); i++) {
-            SPDLOG_INFO("CONCURRENT\tCalling logic [{}/{}]", i, input_vector.size());
-            logic_futures.push_back(std::async(std::launch::async, base_node, input_vector.at(i)));
-        }
-
-        for (int i = 0; i < logic_futures.size(); i++) {
-            SPDLOG_INFO("CONCURRENT\tRetrieving future [{}/{}]", i, logic_futures.size());
-
-            nlohmann::json retrieved_state = logic_futures.at(i).get();
-
-            state_.merge_patch(retrieved_state);
-        }
-
-        return true;
-      }
-    catch (std::exception &e)
-      {
-        SPDLOG_ERROR(e.what());
-        return false;
-      }
-  }
 
   bool add_node(std::vector<node::logic_function> node_vector,node::definition_mode mode_flag);
 
