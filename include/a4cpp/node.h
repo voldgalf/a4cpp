9fdbf6079b8fd8fe2452b6d7e08b4b0f8acd0e33
Voldgalf <50calibursword@gmail.com>
2026-07-06 18:26:54 -0400

    style: reformatted codebase

--- include/a4cpp/node.h
+++ include/a4cpp/node.h
@@ -9,65 +9,62 @@
 
 
 namespace a4cpp::node {
+    /**
+     * @var logic_function
+     * @brief a type alias defining a ```std::function``` with the return type ```nlohmann::json``` and single ```nlohmann::json``` parameter ```state```
+     */
+    using logic_function = std::function<nlohmann::json(nlohmann::json state)>;
 
-/**
- * @var logic_function
- * @brief a type alias defining a ```std::function``` with the return type ```nlohmann::json``` and single ```nlohmann::json``` parameter ```state```
- */
-using logic_function = std::function<nlohmann::json(nlohmann::json state)>;
+    /**
+     * @enum logic_status
+     * @brief The status of logic_data
+     * @property IDLE - not executed yet
+     * @property SUCCESS - successful execution
+     * @property FAILURE - unsuccessful execution
+     *
+     */
+    enum logic_status {
+        IDLE,
+        SUCCESS,
+        FAILURE,
+    };
 
-/**
- * @enum logic_status
- * @brief The status of logic_data
- * @property IDLE - not executed yet
- * @property SUCCESS - successful execution
- * @property FAILURE - unsuccessful execution
- *
- */
-enum logic_status
-{
-  IDLE,
-  SUCCESS,
-  FAILURE,
-};
+    /**
+     * @struct logic_data
+     * @brief Contains logic's ```status``` and ```function```
+     * @property status - the status of the ```function``` property in relation to execution
+     * @property function - an instance of type alias ``logic_function```
+     */
+    struct logic_data {
+        logic_status status = IDLE;
+        logic_function function;
+    };
 
-/**
- * @struct logic_data
- * @brief Contains logic's ```status``` and ```function```
- * @property status - the status of the ```function``` property in relation to execution
- * @property function - an instance of type alias ``logic_function```
- */
-struct logic_data
-{
-  logic_status status = IDLE;
-  logic_function function;
-};
+    /**
+     * @var definition_vector
+     * @brief a type alias defining a std::vector of ```logic_data```
+     */
+    using definition_vector = std::vector<logic_data>;
 
-/**
- * @var definition_vector
- * @brief a type alias defining a std::vector of ```logic_data```
- */
-using definition_vector = std::vector<logic_data>;
+    /**
+     * @enum definition_mode
+     * @brief Determines how ```node_executor::run``` will handle the relative ```definition```
+     * @property SEQUENTIAL - will run in sequence (1,2,etc)
+     * @property CONCURRENT - will run in concurrency (all at once)
+     */
+    enum definition_mode {
+        SEQUENTIAL,
+        CONCURRENT,
+    };
 
-/**
- * @enum definition_mode
- * @brief Determines how ```node_executor::run``` will handle the relative ```definition```
- * @property SEQUENTIAL - will run in sequence (1,2,etc)
- * @property CONCURRENT - will run in concurrency (all at once)
- */
-enum definition_mode {
-  SEQUENTIAL,
-  CONCURRENT,
-};
-
-/**
- * @struct definition
- * @brief Contains the node's ```mode``` and ```logic_vector``` \n IMPORTANT if the ```mode``` is sequential: only the first vector element passed will be called
- * @property mode - either sequential or concurrent
- * @property logic_vector - a vector of ``logic_data```
- */
-struct definition {
-  definition_mode mode;
-  definition_vector logic_vector;
-};
+    /**
+     * @struct definition
+     * @brief Contains the node's ```mode``` and ```logic_vector``` \n IMPORTANT if the ```mode``` is sequential: only the first vector element passed will be called
+     * @property mode - either sequential or concurrent
+     * @property logic_vector - a vector of ``logic_data```
+     */
+    struct definition {
+        definition_mode mode;
+        definition_vector logic_vector;
+    };
 }
--- include/a4cpp/node_executor.h
+++ include/a4cpp/node_executor.h
@@ -8,43 +8,40 @@
 #include <iostream>
 
 namespace a4cpp {
+    /**
+     * @class node_executor
+     * @brief Handles node execution - ADD MORE
+     */
+    class node_executor {
+    protected:
+        std::vector<node::definition> nodes_;
+        nlohmann::json state_;
 
+    public:
+        node_executor() {
+            spdlog::set_pattern("[%H:%M:%S:%e] [%^%l%$] [%s:%# %!] %v");
+            SPDLOG_INFO("node_executor created successfully.");
+        }
 
-/**
- * @class node_executor
- * @brief Handles node execution - ADD MORE
- */
-class node_executor {
-protected:
-  std::vector<node::definition> nodes_;
-  nlohmann::json state_;
+        /**
+         *
+         *
+         * @brief Creates a new ```node::definition``` based on the ```node_vector``` and ```mode_flag``` parameters
+         * @param node_vector
+         * @param mode_flag
+         * @return true
+         * @return false
+         */
+        bool add_node(std::vector<node::logic_function> node_vector, node::definition_mode mode_flag);
 
-public:
-  node_executor()
-  {
-    spdlog::set_pattern("[%H:%M:%S:%e] [%^%l%$] [%s:%# %!] %v");
-    SPDLOG_INFO("node_executor created successfully.");
-  }
+        /**
+         *
+         * @brief
+         * iterates through the ```node_``` member, calling sequential and concurrent nodes
+         * @return bool
+         */
+        bool run();
 
-  /**
-   *
-   *
-   * @brief Creates a new ```node::definition``` based on the ```node_vector``` and ```mode_flag``` parameters
-   * @param node_vector
-   * @param mode_flag
-   * @return true
-   * @return false
-   */
-  bool add_node(std::vector<node::logic_function> node_vector,node::definition_mode mode_flag);
-
-  /**
-   *
-   * @brief
-   * iterates through the ```node_``` member, calling sequential and concurrent nodes
-   * @return bool
-   */
-  bool run();
-
-  nlohmann::json get_state() const;
-};
+        nlohmann::json get_state() const;
+    };
 }
