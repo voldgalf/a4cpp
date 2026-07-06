364b19a867df708a9871221e638b645f45cd3aa0
Voldgalf <50calibursword@gmail.com>
2026-07-06 08:13:42 -0400

    docs: updated doxygen comments

--- include/a4c/node.h
+++ include/a4c/node.h
@@ -1,4 +1,8 @@
 #pragma once
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+
+#include "../nlohmann/json.hpp"
 #include "spdlog/spdlog.h"
 #include <functional>
 #include <vector>
@@ -6,8 +10,20 @@
 
 namespace a4c::node {
 
+/**
+ * @var logic_function
+ * @brief a type alias defining a ```std::function``` with the return type ```nlohmann::json``` and single ```nlohmann::json``` parameter ```state```
+ */
 using logic_function = std::function<nlohmann::json(nlohmann::json state)>;
 
+/**
+ * @enum logic_status
+ * @brief The status of logic_data
+ * @property IDLE - not executed yet
+ * @property SUCCESS - successful execution
+ * @property FAILURE - unsuccessful execution
+ *
+ */
 enum logic_status
 {
   IDLE,
@@ -15,19 +31,41 @@ enum logic_status
   FAILURE,
 };
 
+/**
+ * @struct logic_data
+ * @brief Contains logic's ```status``` and ```function```
+ * @property status - the status of the ```function``` property in relation to execution
+ * @property function - an instance of type alias ``logic_function```
+ */
 struct logic_data
 {
   logic_status status = IDLE;
   logic_function function;
 };
 
+/**
+ * @var definition_vector
+ * @brief a type alias defining a std::vector of ```logic_data```
+ */
 using definition_vector = std::vector<logic_data>;
 
+/**
+ * @enum definition_mode
+ * @brief Determines how ```node_executor::run``` will handle the relative ```definition```
+ * @property SEQUENTIAL - will run in sequence (1,2,etc)
+ * @property CONCURRENT - will run in concurrency (all at once)
+ */
 enum definition_mode {
   SEQUENTIAL,
   CONCURRENT,
 };
 
+/**
+ * @struct definition
+ * @brief Contains the node's ```mode``` and ```logic_vector``` \n IMPORTANT if the ```mode``` is sequential: only the first vector element passed will be called
+ * @property mode - either sequential or concurrent
+ * @property logic_vector - a vector of ``logic_data```
+ */
 struct definition {
   definition_mode mode;
   definition_vector logic_vector;
--- include/a4c/node_executor.h
+++ include/a4c/node_executor.h
@@ -1,16 +1,19 @@
 #pragma once
-
 #include <functional>
 #include <vector>
 #include <future>
 
-#include "../nlohmann/json.hpp"
 #include "./node.h"
 
 #include <iostream>
 
 namespace a4c {
 
+
+/**
+ * @class node_executor
+ * @brief Handles node execution - ADD MORE
+ */
 class node_executor {
 protected:
   std::vector<node::definition> nodes_;
@@ -19,14 +22,27 @@ protected:
 public:
   node_executor()
   {
-
-    spdlog::set_pattern("[%H:%M:%S] [%^%l%$] [%s:%# %!] %v");
+    spdlog::set_pattern("[%H:%M:%S:%e] [%^%l%$] [%s:%# %!] %v");
     SPDLOG_INFO("node_executor created successfully.");
   }
 
-
+  /**
+   *
+   *
+   * @brief Creates a new ```node::definition``` based on the ```node_vector``` and ```mode_flag``` parameters
+   * @param node_vector
+   * @param mode_flag
+   * @return true
+   * @return false
+   */
   bool add_node(std::vector<node::logic_function> node_vector,node::definition_mode mode_flag);
 
+  /**
+   *
+   * @brief
+   * iterates through the ```node_``` member, calling sequential and concurrent nodes
+   * @return bool
+   */
   bool run();
 
   nlohmann::json get_state() const;
